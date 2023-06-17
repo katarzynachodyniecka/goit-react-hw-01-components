886f4c80e1ca053f7ece87722f4824988965476d
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-06-17 13:33:40 +0200

    added heading in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,12 @@ import friends from '../utils/friends.json';
 import user from '../utils/user.json';
 import data from '../utils/data.json';
 import transactions from '../utils/transactions.json';
+import styles from './App.module.css';
 
 export const App = () => {
   return (
     <div>
+      <span className={styles.heading}>Zadanie 1</span>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -17,8 +19,11 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <span className={styles.heading}>Zadanie 2</span>
       <Statistics title="Upload stats" data={data} />
+      <span className={styles.heading}>Zadanie 3</span>
       <FriendList friends={friends} />
+      <span className={styles.heading}>Zadanie 4</span>
       <Transactions items={transactions} />
     </div>
   );
