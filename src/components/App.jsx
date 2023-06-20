e957422fcbad6d80e16c0ca048bdb750d9c4d7e8
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-06-20 15:00:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './Friends/FriendList';
+import { Transactions } from './Transactions/Transactions';
 import friends from '../utils/friends.json';
 import user from '../utils/user.json';
 import data from '../utils/data.json';
+import transactions from '../utils/transactions.json';
 
 export const App = () => {
   return (
@@ -17,6 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" data={data} />
       <FriendList friends={friends} />
+      <Transactions items={transactions} />
     </div>
   );
 };
